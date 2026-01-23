b367adf08e2d88f73ec708a1a8193cec1698e2d3
RavinduThennakoon <tennakoonravindu79@gmail.com>
2026-01-23 18:08:31 +0530

    Sqaure-Game-push

---
+++ Square_Game/Square_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Square_GameApp.swift
+//  Square_Game
+//
+//  Created by COBSCCOMP242P-017 on 2026-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Square_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
